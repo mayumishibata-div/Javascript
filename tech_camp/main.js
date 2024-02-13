4e67d7687eae0880d0cde80f5ddce190dc1e9251
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-13 16:32:19 +0900

    条件分岐

--- tech_camp/main.js
+++ tech_camp/main.js
@@ -9,3 +9,20 @@ const nickname = "TOM"
 const profile = (`私の名前は${nickname}です。年齢は${age}です`)
 console.log(profile)
 
+// 条件分岐
+const num = 60
+if (num%15==0) {
+  console.log(`${num}は3と5の倍数です`)
+}
+else if (num%3==0) {
+  console.log(`${num}は3の倍数です`)
+}
+
+else if (num%5==0){
+  console.log(`${num}は5の倍数です`)
+}
+
+else {
+  console.log(`${num}は3でも5でもありません`)
+}
+
