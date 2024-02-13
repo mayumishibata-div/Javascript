eec8f95c710e2e62a31bbe9dd28d6d1c4f4c8efb
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-13 16:33:05 +0900

    繰り返し処理

--- tech_camp/main.js
+++ tech_camp/main.js
@@ -32,3 +32,23 @@ console.log(list)
 console.log(list[2])
 //  みかん
 
+// 繰り返し処理
+// for文を使う
+// for ([初期化式]; [条件式]; [加算式]){
+//   繰り返す内容
+// }
+let count = 1
+for(let i = 1; i<=10; i+=1){
+  console.log(`${count}回目の処理です`)
+  count +=1
+}
+
+// 配列の繰り返し処理
+// forEach(function(){
+  // 処理の内容
+// })
+
+const fruit = ["りんご", "バナナ","パイナップル"]
+fruit.forEach(function(item){
+  console.log(`今日の果物は${item}です`)
+})
