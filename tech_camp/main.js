7b294ca029dfa4f5607581ea1ddd0918ae37e79a
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-13 18:18:41 +0900

    様々な関数定義

--- tech_camp/main.js
+++ tech_camp/main.js
@@ -75,3 +75,78 @@ const num1 = 2
 const num2 = 3
 
 console.log(calc(num1,num2))
+
+// 関数定義
+
+  // 関数宣言
+  // 標準的な関数の定義。
+    // function 関数名( 引数 ){
+    //   // 関数内の処理
+    // }
+    function study(subject, hour) {
+      return console.log(`今日は${subject}の学習を${hour}時間しました`)
+    }
+    let subject = "english"
+    let hour = 1
+
+    study(subject, hour)
+
+  // 関数式
+  // 初めの段階で変数宣言が必要がどうかの違い
+      // 変数 = function( 引数 ){
+        // 関数内の処理
+      // }
+
+    study = function (subject2, hour2){
+      return console.log(`今日は${subject2}の学習を${hour2}時間しました`)
+    }
+
+    let subject2 = "プログラミング"
+    let hour2 = 1
+
+    study(subject2, hour2)
+
+  // 無名関数
+  // 関数を多く使用するコードであるときに使用する。関数名の重複を避けることができる。
+    // 関数名なしで関数を定義することができる
+    // より簡潔なコードが書ける
+
+    const brainstorming = function(subject3, hour3){
+      return console.log(`今日は${subject3}の学習を${hour3}時間しました`)
+    }
+
+    let subject3 = "JavaScript"
+    let hour3 = 4
+
+    brainstorming(subject3, hour3)
+
+  // 即時関数
+  // 流用する可能性のない関数を定義するときに使用する。別途関数を定義する手間がない。
+    // 定義したと同時に実行される
+    // 関数を定義する手間が省ける
+    
+
+    const self_taught = function(subject4, hour4) {
+      return console.log(`今日は${subject4}の学習を${hour4}時間しました`)
+    }
+
+    let subject4 = "関数"
+    let hour4 = 4
+    
+    self_taught(subject4, hour4)
+  
+  // アロー関数
+  // 無名関数または即時関数において、より省略した記述をしたい時に使用する。
+    // functionを省略　()=>
+      // const 変数名 = () => {
+      //   処理
+      // }
+
+        const effort = (subject5, hour5)=> {
+          return console.log(`今日は${subject5}の学習を${hour5}時間しました`)
+        }
+    
+        let subject5 = "コーチング"
+        let hour5 = 4
+        
+        effort(subject5, hour5)
