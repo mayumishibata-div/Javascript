a5c87e2929a10ea94fc67c036d05205bfaa4ff1d
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-13 16:31:58 +0900

    コンソールの練習

---
+++ tech_camp/main.js
@@ -0,0 +1,11 @@
+const hello = "おはよう"
+console.log(hello)
+
+const fruits = "りんご"
+console.log(fruits)
+
+const age = 25
+const nickname = "TOM"
+const profile = (`私の名前は${nickname}です。年齢は${age}です`)
+console.log(profile)
+
