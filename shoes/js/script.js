538e6e7b6b49270d76c1268793917ec9dc3960c6
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-03-23 21:57:50 +0900

    スライド機能の実装

--- shoes/js/script.js
+++ shoes/js/script.js
@@ -1,3 +1,4 @@
+
 const ham = document.querySelector("#js-hamburger")
 const nav = document.querySelector("#js-nav")
 
@@ -5,4 +6,18 @@ ham.addEventListener("click",function(){
   ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
   nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
 })
+
+// スライダー
+const swiper = new Swiper(".mySwiper",{
+  slidesPerView: 2,
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: true,
+  breakpoints: {
+    750: {
+      slidesPerView: 4,
+      spaceBetween: 60,
+    },
+  }
+});
 
