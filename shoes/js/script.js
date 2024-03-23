f8e9303adab2201c9aeddd5462eb967cadd6a5da
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-03-23 23:49:14 +0900

    フェードイン

--- shoes/js/script.js
+++ shoes/js/script.js
@@ -20,4 +20,24 @@ const swiper = new Swiper(".mySwiper",{
     },
   }
 });
+// フェードイン
+// スクロール時のイベント
+window.addEventListener("scroll", function(){
+
+  // 全ての.feature-contentを取得
+  const item = document.querySelectorAll(".feature-content");
+  // 配列になるのでfor構文を使用
+  for(let i = 0; i < item.length; i++){
+// .feature-contentのオフセット高さを取得
+let targetTop = item[i].offsetTop;
+// 画面のスクロール量 + 300px >.feature-contentのオフセット高さを取得
+  if (window.scrollY + 300 > targetTop){
+    item[i].classList.add("show")
+  };
+    // console.log(targetTop)
+    // console.log(window.scrollY + 300 > targetTop)
+  }
+});
+
+
 
